95632931fd77713fdc95ace3698ce7dc975ac03c
Loïck MAHIEUX <loick111@gmail.com>
2017-03-30 22:42:50 +0200

    initial commit

---
+++ src/test/java/arquillian/AbstractTCFTest.java
@@ -0,0 +1,19 @@
+package arquillian;
+
+import fr.unice.polytech.isa.tcf.components.CatalogueBean;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+public abstract class AbstractTCFTest {
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        // Building a Web ARchive (WAR) containing the following elements:
+        return ShrinkWrap.create(WebArchive.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addPackage(CatalogueBean.class.getPackage());
+    }
+
+}
